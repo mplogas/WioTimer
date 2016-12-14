c8264cc26ee6c2847b89d2d0149f713228cd168e
Marc Plogas <marc.plogas@digitalnatives.de>
2016-12-14 13:05:46 +0100

    initial seed

---
+++ WioTimer/ConfigReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+
+namespace WioTimer
+{
+    public class ConfigReader
+    {
+        public static T GetValue<T>(string key)
+        {
+            var config = ConfigurationManager.AppSettings.Get(key);
+            if (!string.IsNullOrEmpty(config))
+            {
+                return (T) Convert.ChangeType(config, typeof(T));
+            }
+
+            return default(T);
+        } 
+    }
+}
---
+++ WioTimer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WioTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logic = new Logic(new HttpClient());
+            logic.Connect();
+
+            var input = Console.ReadLine();
+            if (!string.IsNullOrEmpty(input) && input.Equals("stop", StringComparison.InvariantCultureIgnoreCase))
+            {
+                logic.Dispose();
+                return;
+            }
+        }
+        
+    }
+}
